c08ae6f4dbfcb2fa844e66512fd2d7c45422e0aa
Gil Marques <gilmmm4@gmail.com>
2021-05-09 16:06:27 +0100

    qq

--- TP2/run.py
+++ TP2/run.py
@@ -2,9 +2,18 @@ import os
 import subprocess
 
 os.system('make')
-os.system('rm out.txt')
-os.system('touch out.txt')
+os.system('rm outinsert.txt')
+os.system('rm outquick.txt')
+os.system('rm outinsert2.txt')
+
 n = 1000000
 
 for i in range(2,33,2):
-    os.system('perf stat -o out.txt --append ./insertparallel %d %d' % (n, i))
+    os.system('perf stat -o outinsert.txt --append ./insertparallel %d %d' % (n, i))
+
+
+for i in range(2,33,2):
+    os.system('perf stat -o outquick.txt --append ./quickparallel %d %d' % (n, i))
+
+for i in range(2,33,2):
+    os.system('perf stat -o outinsert2.txt --append ./insertparallel2 %d %d' % (n, i))
