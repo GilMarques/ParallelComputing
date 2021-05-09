4c8c23b54f64b2ca06c697292ad634d9107c15c0
Gil Marques <gilmmm4@gmail.com>
2021-05-09 18:12:21 +0100

    qq

--- TP2/run.py
+++ TP2/run.py
@@ -3,23 +3,28 @@ import subprocess
 import sys
 os.system('make')
 
+os.system('mkdir results')
 
-if(len(sys.argv)>2):
-    size = int(sys.argv[1])
-    n_buckets = int(sys.argv[2])
-    folder = 'results/s%d_nb%d'%(size, n_buckets)
+
+size = 1000000
+n_buckets = 1000
+while n_buckets<=100000:
+
+    folder = 'results/s%d_nb%d' % (size, n_buckets)
     os.system('mkdir %s' % (folder))
 
-    os.system('perf stat -o %s/out_insert_sequential.txt --append ./insertsequential %d %d' % (folder,size,n_buckets))
-    os.system('perf stat -o %s/out_quick_sequential.txt --append ./quicksequential %d %d' % (folder,size,n_buckets))
-    for n_threads in range(2,33,2):
-        os.system('perf stat -o %s/out_insert_parallel.txt --append ./insertparallel %d %d %d' % (folder,size,n_buckets ,n_threads))
+    os.system('perf stat -o %s/out_insert_sequential.txt --append ./insertsequential %d %d' %
+                (folder, size, n_buckets))
+    os.system('perf stat -o %s/out_quick_sequential.txt --append ./quicksequential %d %d' %
+                (folder, size, n_buckets))
+    for n_threads in range(2, 33, 2):
+            os.system('perf stat -o %s/out_insert_parallel.txt --append ./insertparallel %d %d %d' %
+                    (folder, size, n_buckets, n_threads))
 
+    for n_threads in range(2, 33, 2):
+        os.system('perf stat -o %s/out_quick_parallel.txt --append ./quickparallel %d %d %d' %
+                    (folder, size, n_buckets, n_threads))
+    n_buckets = n_buckets*10
 
-    for n_threads in range(2,33,2):
-        os.system('perf stat -o %s/out_quick_parallel.txt --append ./quickparallel %d %d %d' % (folder,size,n_buckets,n_threads))
-else:
-    print('No')
-#for i in range(2,33,2):
+# for i in range(2,33,2):
 #    os.system('perf stat -o results/outinsert2.txt --append ./insertparallel2 %d %d' % (n, i))
-
